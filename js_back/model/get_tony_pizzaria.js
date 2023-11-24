76c8ea8a1578fbdc7fbcd2d48c4ff9cb7eb2296c
Vitoria <vazevedo73@gmail.com>
2023-11-24 17:24:31 -0300

    .

---
+++ js_back/model/get_tony_pizzaria.js
@@ -0,0 +1,20 @@
+var tony_pizzaria = require('./tony_pizzaria')
+
+const getCategorias = function(){
+    listaCategoriasJSON = {}
+    listaCategoriasARRAY = []
+
+    tony_pizzaria.categorias.categorias.forEach(function(nome){
+        listaCategoriasARRAY.push(nome.nome)
+    })
+
+    listaCategoriasJSON.categorias = listaCategoriasARRAY
+    console.log(listaCategoriasJSON)
+}
+
+const getDadosProdutos = function(){
+    listaProdutosJSON = {}
+    listaProdutosARRAY
+}
+
+getCategorias()
